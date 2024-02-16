2abfd5c8d5acf8fabd402b06ce13b7e8f5d4c475
ajiinisti <ajiinisti@gmail.com>
2024-02-16 14:38:43 +0700

    [FIX] Logs on User Upload & Search User Name/NIK

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -161,6 +161,7 @@ func (u *userRepo) PaginateList(pagination model.PaginationQuery) ([]model.User,
 			Preload("Roles").
 			Preload("BusinessUnit").
 			Where("name ILIKE ?", "%"+pagination.Name+"%").
+			Or("nik ILIKE ?", "%"+pagination.Name+"%").
 			Limit(pagination.Take).Offset(pagination.Skip).Find(&users).Error
 		if err != nil {
 			return nil, response.Paging{}, err
@@ -230,7 +231,7 @@ func (u *userRepo) PaginateByProjectId(pagination model.PaginationQuery, project
 			Preload("CalibrationScores.ProjectPhase.Phase").
 			Joins("JOIN actual_scores ON users.id = actual_scores.employee_id AND actual_scores.deleted_at IS NULL").
 			Joins("LEFT JOIN calibrations ON users.id = calibrations.employee_id").
-			Where("(actual_scores.project_id = ? OR calibrations.project_id = ?) AND name ILIKE ?", projectId, projectId, "%"+pagination.Name+"%").
+			Where("(actual_scores.project_id = ? OR calibrations.project_id = ?) AND (name ILIKE ? OR nik ILIKE ?)", projectId, projectId, "%"+pagination.Name+"%", "%"+pagination.Name+"%").
 			Group("users.id").
 			Limit(pagination.Take).Offset(pagination.Skip).
 			Find(&users).Error
