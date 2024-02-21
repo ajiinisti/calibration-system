7dd89af27bc0cce0cc1bf917a4ed8f16702ccc78
ajiinisti <ajiinisti@gmail.com>
2024-02-21 20:37:22 +0700

    [FIX] Wa Number Empty

--- utils/whatsapp_notif.go
+++ utils/whatsapp_notif.go
@@ -11,19 +11,21 @@ import (
 
 func SendWhatsAppNotif(config config.WhatsAppConfig, phoneNumber, data1, data2, data3 string) error {
 	// Ganti nomor telpon ke phoneNumber, ini hanya untuk UAT
-	formData := bytes.NewBufferString(fmt.Sprintf(`message={"11": "%s","22": "%s","33": "%s"}&template_id=%s&api_key=%s&shorten_url=%s&to_no=%s`,
-		data1, data2, data3, config.TemplateID, config.ApiKey, config.ShortenUrl, phoneNumber))
-	fmt.Println(formData)
-	resp2, err := http.Post(config.URL, "application/x-www-form-urlencoded", formData)
-	if err != nil {
-		// handle error
-		return err
-	}
+	if phoneNumber != "" {
+		formData := bytes.NewBufferString(fmt.Sprintf(`message={"11": "%s","22": "%s","33": "%s"}&template_id=%s&api_key=%s&shorten_url=%s&to_no=%s`,
+			data1, data2, data3, config.TemplateID, config.ApiKey, config.ShortenUrl, phoneNumber))
+		fmt.Println(formData)
+		resp2, err := http.Post(config.URL, "application/x-www-form-urlencoded", formData)
+		if err != nil {
+			// handle error
+			return err
+		}
 
-	rbody2, err := ioutil.ReadAll(resp2.Body)
-	if err != nil {
-		fmt.Println("Error pesan", err.Error())
+		rbody2, err := ioutil.ReadAll(resp2.Body)
+		if err != nil {
+			fmt.Println("Error pesan", err.Error())
+		}
+		fmt.Println(string(rbody2))
 	}
-	fmt.Println(string(rbody2))
 	return nil
 }
