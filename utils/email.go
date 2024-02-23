b42c5d580c09a886bda97afd33ff217a1d74556f
ajiinisti <ajiinisti@gmail.com>
2024-02-24 03:33:52 +0700

    [FIX] EMail not send

--- utils/email.go
+++ utils/email.go
@@ -25,40 +25,42 @@ type EmailData struct {
 }
 
 func SendMail(to []string, data *EmailData, templateDir string, templatePath string, cfg config.SMTPConfig) error {
-	emailTemplate, err := ParseTemplateFile(templateDir, templatePath)
-	if err != nil {
-		return fmt.Errorf("Could not parse email template: " + err.Error())
-	}
+	if len(to) > 0 {
+		emailTemplate, err := ParseTemplateFile(templateDir, templatePath)
+		if err != nil {
+			return fmt.Errorf("Could not parse email template: " + err.Error())
+		}
 
-	var emailContent bytes.Buffer
-	err = emailTemplate.Execute(&emailContent, &data)
-	if err != nil {
-		return fmt.Errorf("Error executing template: " + err.Error())
-	}
+		var emailContent bytes.Buffer
+		err = emailTemplate.Execute(&emailContent, &data)
+		if err != nil {
+			return fmt.Errorf("Error executing template: " + err.Error())
+		}
 
-	mailer := gomail.NewMessage()
-	mailer.SetHeader("From", cfg.SMTPSenderName)
-	mailer.SetHeader("To", to...)
-	mailer.SetHeader("Subject", data.Subject)
-	fmt.Println("SCONTENT BODY", emailContent.String())
-	mailer.SetBody("text/html", emailContent.String())
-	// mailer.AddAlternative("text/plain", html2text.HTML2Text(emailContent.String()))
+		mailer := gomail.NewMessage()
+		mailer.SetHeader("From", cfg.SMTPSenderName)
+		mailer.SetHeader("To", to...)
+		mailer.SetHeader("Subject", data.Subject)
+		fmt.Println("SCONTENT BODY", emailContent.String())
+		mailer.SetBody("text/html", emailContent.String())
+		// mailer.AddAlternative("text/plain", html2text.HTML2Text(emailContent.String()))
 
-	port, err := strconv.Atoi(cfg.SMTPPort)
-	if err != nil {
-		return err
-	}
+		port, err := strconv.Atoi(cfg.SMTPPort)
+		if err != nil {
+			return err
+		}
 
-	dialer := gomail.NewDialer(
-		cfg.SMTPHost,
-		port,
-		cfg.SMTPEmail,
-		cfg.SMTPPassword,
-	)
-	dialer.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+		dialer := gomail.NewDialer(
+			cfg.SMTPHost,
+			port,
+			cfg.SMTPEmail,
+			cfg.SMTPPassword,
+		)
+		dialer.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 
-	if err := dialer.DialAndSend(mailer); err != nil {
-		return err
+		if err := dialer.DialAndSend(mailer); err != nil {
+			return err
+		}
 	}
 	return nil
 }
