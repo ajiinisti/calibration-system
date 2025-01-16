55e824acce0ad26b43dae4712a766d63e56b6546
ajiinisti <ajiinisti@gmail.com>
2025-01-16 09:57:14 +0700

    [FIX] Error Summary

--- usecase/project_usecase.go
+++ usecase/project_usecase.go
@@ -506,12 +506,11 @@ func (r *projectUsecase) FindSummaryProjectByCalibratorID(calibratorID, projectI
 	finalData := map[string]*response.BusinessUnitTotal{}
 	for _, summary := range summaries {
 		types := "all"
-		if summary.CalibratorName == "N-1" {
-			types = "n-1"
-		}
+		// if summary.CalibratorName == "N-1" {
+		// 	types = "n-1"
+		// }
 
 		guidance, err := r.FindRatingQuotaByCalibratorIDforSummaryHelper(calibratorID, summary.CalibratorID, summary.CalibratorBusinessUnitID, types, projectID, 0)
-		// guidance, err := r.FindRatingQuotaByCalibratorID(calibratorID, summary.CalibratorID, summary.CalibratorBusinessUnitID, types, projectID, 0)
 		if err != nil {
 			return nil, err
 		}
